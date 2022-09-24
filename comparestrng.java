e0a1599a36abe6db5df3d0878d5b634c39a1bb0b
LavanyaB1605 <lavanya.bhaskaran16@gmail.com>
2022-09-24 07:41:12 +0530

    added files

---
+++ comparestrng.java
@@ -0,0 +1,9 @@
+public class comparestrng {
+    public static void main(String args[]){
+        String s1="JavaString";
+        String s2="JavaString";
+        String s3="String";
+        System.out.println(s1==s2);
+        System.out.println(s1==s3);
+    }
+}
---
+++ defaultvalue.java
@@ -0,0 +1,17 @@
+public class defaultvalue {
+    static boolean a;
+    static double b;
+    static float c;
+    static int d;
+    static long e;
+    static String f;
+    public static void main(String[] args) {
+        System.out.println("Default value of Primitive datatypes:");
+        System.out.println("Boolean = " + a);
+        System.out.println("Double = " + b);
+        System.out.println("Float = " + c);
+        System.out.println("Integer = " + d);
+        System.out.println("Long = " + e);
+        System.out.println("String = " + f);
+    }
+}
